11b800591321993b9ada69b206d763870a979905
savanna chiang <savannaac@gmail.com>
2021-02-12 17:26:28 -0600

    Cash Register - Complete

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+class CashRegister
+    attr_reader :discount, :items
+    attr_accessor :total
 
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(item, price, quantity=1)
+        quantity.times do
+            @items << item
+        end
+        @last_transaction = price * quantity
+        @total += @last_transaction
+    end
+
+    def apply_discount
+        if @discount == 0
+            "There is no discount to apply."
+        else
+            @total = @total - (@total * @discount /100)
+            "After the discount, the total comes to $#{@total}."
+        end
+    end
+
+    def void_last_transaction
+        @total -= @last_transaction
+    end
+end
